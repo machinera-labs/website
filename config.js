f90969cce9d16317554e6712d31d233a3c661540
admin <admin@machinera.com>
2026-01-09 12:24:57 -0800

    website

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+window.SiteConfig = {
+    companyName: "Machinera LLC",
+    domain: "machinera.com",
+    emailUser: "contact",
+    
+    // Your iPostal1 Address (Use \n for line breaks)
+    address: "490 Post Street, Suite 500-2302\nSan Francisco, CA 94102\nUnited States",
+    
+    // Copyright Year
+    year: new Date().getFullYear()
+};
